bc58f4752ea4feb518fd9d4868a1e37c9d52ee84
José Miguel Moreno <josemmo@pm.me>
2020-10-26 16:22:56 +0100

    Fixed ssdeep.test.js
    - Hoisted index variable on test creation

--- ssdeep.test.js
+++ ssdeep.test.js
@@ -5,15 +5,19 @@ var ssdeep = require('.');
 
 var NUM_OF_SAMPLES = 3;
 
+function createTest(index) {
+    var paddedId = (index < 10) ? '0'+index : index+'';
+    var basePath = path.resolve(__dirname, 'test', 'sample-'+paddedId);
+    it('produces the correct digest for ' + basePath + '.dat', function() {
+        var sampleData = fs.readFileSync(basePath + '.dat');
+        var expectedHash = fs.readFileSync(basePath + '.hash').toString();
+        var actualHash = ssdeep.digest(sampleData);
+        assert.strictEqual(actualHash, expectedHash);
+    });
+}
+
 describe('ssdeep', function() {
     for (var i=1; i<=NUM_OF_SAMPLES; i++) {
-        var paddedId = (i < 10) ? '0'+i : i+'';
-        var basePath = path.resolve(__dirname, 'test', 'sample-'+paddedId);
-        it('produces the correct digest for ' + basePath + '.dat', function() {
-            var sampleData = fs.readFileSync(basePath + '.dat');
-            var expectedHash = fs.readFileSync(basePath + '.hash').toString();
-            var actualHash = ssdeep.digest(sampleData);
-            assert.strictEqual(actualHash, expectedHash);
-        });
+        createTest(i);
     }
 });
